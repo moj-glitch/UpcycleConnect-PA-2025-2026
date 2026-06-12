68d0a057090d12ce680f6b44a5cfb58ea13434c0
Julian Lavis--Fabbri <leviathancorp.officiel@gmail.com>
2026-06-12 16:38:23 +0200

    Add account creation endpoint to OAuth server

--- oauth/main.go
+++ oauth/main.go
@@ -12,6 +12,7 @@ const DATABASE_PRO_URL string = "postgres://postgres:035464@localhost:5432/datab
 const DATABASE_ADMIN_URL string = "postgres://postgres:035464@localhost:5432/database"
 
 func main() {
+	http.HandleFunc("/oauth/v3/inscription", createAccount)
 	http.HandleFunc("/oauth/v3/token", token)
 	http.HandleFunc("/oauth/v3/introspect", introspect)
 	err := http.ListenAndServe(":80", nil)
