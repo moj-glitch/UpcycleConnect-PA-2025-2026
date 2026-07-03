14ece214b14e9b25fb59c23017731dfc611f08c5
Julian Lavis--Fabbri <leviathancorp.officiel@gmail.com>
2026-07-03 11:12:22 +0200

    ref/ added ENV vars and roles endpoint

--- oauth/main.go
+++ oauth/main.go
@@ -5,17 +5,24 @@ import (
 	"net/http"
 )
 
-const DATABASE_AUTH_URL string = "postgres://postgres:035464@localhost:5432/database"
-const DATABASE_DATA_URL string = "postgres://postgres:035464@localhost:5432/database"
-const DATABASE_PUBLIC_URL string = "postgres://postgres:035464@localhost:5432/database"
-const DATABASE_PRO_URL string = "postgres://postgres:035464@localhost:5432/database"
-const DATABASE_ADMIN_URL string = "postgres://postgres:035464@localhost:5432/database"
+var (
+	DATABASE_AUTH_URL   = getEnv("DATABASE_AUTH_URL", "postgres://postgres:035464@localhost:5432/database")
+	DATABASE_DATA_URL   = getEnv("DATABASE_DATA_URL", DATABASE_AUTH_URL)
+	DATABASE_PUBLIC_URL = getEnv("DATABASE_PUBLIC_URL", DATABASE_AUTH_URL)
+	DATABASE_PRO_URL    = getEnv("DATABASE_PRO_URL", DATABASE_AUTH_URL)
+	DATABASE_ADMIN_URL  = getEnv("DATABASE_ADMIN_URL", DATABASE_AUTH_URL)
+)
 
 func main() {
 	http.HandleFunc("/oauth/v3/inscription", createAccount)
 	http.HandleFunc("/oauth/v3/token", token)
 	http.HandleFunc("/oauth/v3/introspect", introspect)
-	err := http.ListenAndServe(":80", nil)
+	http.HandleFunc("/oauth/v3/roles", roles)
+
+	port := getEnv("PORT", "8080")
+	fmt.Println("Listening on :" + port)
+
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
